af4d4bea55648d9b59d2b9782775d306e4d00477
flogrv <florian.grave@epitech.eu>
2024-03-25 03:33:28 +0100

    :sparkles: Ajout du gestionnaire de plugins lazy et d'un theme

--- init.lua
+++ init.lua
@@ -41,3 +41,33 @@ vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- etc etc... voir :help option-list
+--
+
+-- Etape 3 ) Installtion de lazy vim avec un theme
+--
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    }
+
+}
+local opts = {}
+require("lazy").setup(plugins, opts)
+vim.cmd[[colorscheme tokyonight]]
